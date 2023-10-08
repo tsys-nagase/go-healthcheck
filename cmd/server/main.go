b62615734e426ed8f1922add35ae66b2b5ca9019
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2023-10-08 23:26:10 +0900

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	// (一部抜粋)
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
+	hellopb "example/hello/pkg/grpc"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type myServer struct {
 	hellopb.UnimplementedGreetingServiceServer
 }
+
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
 	// リクエストからnameフィールドを取り出して
 	// "Hello, [名前]!"というレスポンスを返す
+	fmt.Println("Helloが呼ばれました..", req.GetName())
 	return &hellopb.HelloResponse{
 		Message: fmt.Sprintf("🐔Hello, %s!", req.GetName()),
 	}, nil
 }
 
+// 自作サービス構造体のコンストラクタを定義
+func NewMyServer() *myServer {
+	return &myServer{}
+}
 
 func main() {
 	// 1. 8080番portのLisnterを作成
@@ -34,10 +44,12 @@ func main() {
 	// 2. gRPCサーバーを作成
 	s := grpc.NewServer()
 
-
 	// 3. gRPCサーバーにGreetingServiceを登録
-	hellopb.RegisterGreetingServiceServer(s, [サーバーに登録するサービス])
+	// hellopb.RegisterGreetingServiceServer(s, [サーバーに登録するサービス])
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
 
+	// 4. サーバーリフレクションの設定
+	reflection.Register(s)
 
 	// 3. 作成したgRPCサーバーを、8080番ポートで稼働させる
 	go func() {
